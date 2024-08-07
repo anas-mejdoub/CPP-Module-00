aa3352f2a3e4d180389ed621d31cf1f04bb3e643
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-08-07 13:19:28 +0100

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 12:31:39 by amejdoub          #+#    #+#             */
+/*   Updated: 2024/08/07 12:34:31 by amejdoub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    if (ac < 2)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
+    else
+    {
+        for (int i = 1; i < ac; i++)
+        {
+            for (int j = 0; av[i][j]; j++)
+            {
+                std::cout << (char)toupper(av[i][j]);
+            }
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
