cfe6b5b2f4ac9d7055451fb4b5d5211a8f2a9990
anas-mejdoub <anasmejdoub2000@gmail.com>
2024-08-08 14:32:21 +0100

    fix makefile

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: amejdoub <amejdoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 13:05:25 by amejdoub          #+#    #+#             */
-/*   Updated: 2024/08/08 13:17:13 by amejdoub         ###   ########.fr       */
+/*   Updated: 2024/08/08 14:06:42 by amejdoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 PhoneBook::PhoneBook()
 {
     std::cout << "phone just created !\n";
+}
+Contact::Contact()
+{
+    std::cout << "contact\n";
 }
 
 int main()
